8046a919fd48e5d459b8f206dbde43bcaef50087
František Čaník <snipiba@gmail.com>
2021-04-30 14:39:24 +0200

    Update plugin name and langs

--- lang/en/lang.php
+++ lang/en/lang.php
@@ -13,4 +13,18 @@
         'source' => 'Source',
         'source_url' => 'Source URL',
     ],
+    'component' => [
+        'name' => 'Metadata',
+        'description' => 'Show data from media metadata',
+        'file_path' => 'Source file',
+        'file_path_description' => 'Paste path from media library',
+        'tag_label' => 'HTML tag',
+        'tag_label_description' => 'Wrap metadata into this HTML tag',
+        'css_classes_label' => 'CSS classes',
+        'css_classes_label_description' => 'Append this CSS classes to tag',
+        'custom_style_label' => 'Custom styles',
+        'custom_style_label_description' => 'When you need to use style',
+        'field' => 'Fetch data from',
+        'field_description' => 'Choose source of data to show',
+    ],
 ];
